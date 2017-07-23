81b1ee803304859b2f1da6db1a33a3b452eb8597
PaulinaKwiatkowska <paulina.l.kwiatkowska@gmail.com>
2017-07-23 14:04:44 +0200

    2nd commit

--- craters_mars.py
+++ craters_mars.py
@@ -43,6 +43,8 @@ plt.xlabel('diameter of craters [km]')
 plt.ylabel('number of craters')
 plt.title("distribution of craters' diameter")
 plt.xlim(1,10)
+print 'mean diameter:',round(source['DIAM_CIRCLE_IMAGE'].mean(),2),'[km]'
+print 'diameter standard deviation:',round(source['DIAM_CIRCLE_IMAGE'].std(),2),'[km]'
 
 #creating histogram of craters' longitude
 plt.figure()
@@ -54,8 +56,8 @@ plt.title("count of craters' longitude")
 plt.xlim(-180,180)
 plt.xticks(np.arange(-180, 181, 20))
 
-print 'mean:',source['LONGITUDE_CIRCLE_IMAGE'].mean()
-print 'standard deviation:',source['LONGITUDE_CIRCLE_IMAGE'].std()
+print 'mean longitude:',round(source['LONGITUDE_CIRCLE_IMAGE'].mean(),2),'[deg]'
+print 'longitude standard deviation:',round(source['LONGITUDE_CIRCLE_IMAGE'].std(),2),'[deg]'
 
 #creating histogram of craters' latitude
 plt.figure()
@@ -67,5 +69,5 @@ plt.title("count of craters' latitude")
 plt.xticks(np.arange(-90, 91, 10))
 plt.yticks(np.arange(0, 50001, 5000))
 
-print 'mean:',source['LATITUDE_CIRCLE_IMAGE'].mean()
-print 'standard deviation:',source['LATITUDE_CIRCLE_IMAGE'].std()
+print 'mean latitude:',round(source['LATITUDE_CIRCLE_IMAGE'].mean(),2),'[deg]'
+print 'latitude standard deviation:',round(source['LATITUDE_CIRCLE_IMAGE'].std(),2),'[deg]'
